1511d5173e44dc8f9e84ff88cb9100177c432225
Marius <mulamamarius@gmail.com>
2020-06-01 22:46:01 +0300

    Committed final copy

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About </h1>
+      <p>Cocktails site fetching data from from the cocktatils API. Part of react tutorial practice</p>
+      
+    </section>
+  )
+}
+
+export default About
